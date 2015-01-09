3153ec5f18d4e5bb3da32ce2c3364a4d5f50cef5
Jaegeuk Kim <jaegeuk@kernel.org>
2015-01-08 19:11:05 -0800

    xfs: introduce a generic shutdown ioctl
    This patch introduces a generic ioctl for fs shutdown used by xfs.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Al3XKOoL <moonrotation@gmail.com>

--- kernel/include/linux/fs.h
+++ kernel/include/linux/fs.h
@@ -383,6 +383,14 @@ struct inodes_stat_t {
 #define SYNC_FILE_RANGE_WRITE		2
 #define SYNC_FILE_RANGE_WAIT_AFTER	4
 
+/*
+ * Flags for going down operation used by FS_IOC_GOINGDOWN
+ */
+#define FS_GOING_DOWN_FULLSYNC	0x0	/* going down with full sync */
+#define FS_GOING_DOWN_METASYNC	0x1	/* going down with metadata */
+#define FS_GOING_DOWN_NOSYNC	0x2	/* going down */
+
+
 #ifdef __KERNEL__
 
 #include <linux/linkage.h>
