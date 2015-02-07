9cfcfed9891a53661c17ac96d204728ce0bf464a
Al3XKOoL <moonrotation@gmail.com>
2015-02-07 21:15:21 +0000

    HID-Gadget: add bios support
    Signed-off-by: Al3XKOoL <moonrotation@gmail.com>

--- kernel/drivers/usb/gadget/f_hid.c
+++ kernel/drivers/usb/gadget/f_hid.c
@@ -403,7 +403,11 @@ static int hidg_setup(struct usb_function *f,
 	case ((USB_DIR_IN | USB_TYPE_CLASS | USB_RECIP_INTERFACE) << 8
 		  | HID_REQ_GET_PROTOCOL):
 		VDBG(cdev, "get_protocol\n");
-		goto stall;
+		// always respond 0=BOOT Mode !
+		length = 0x1;
+		memset(req->buf, 0x0, length);
+		goto respond;
+		//goto stall;
 		break;
 
 	case ((USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE) << 8
@@ -417,7 +421,10 @@ static int hidg_setup(struct usb_function *f,
 	case ((USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE) << 8
 		  | HID_REQ_SET_PROTOCOL):
 		VDBG(cdev, "set_protocol\n");
-		goto stall;
+		// always respond OK !
+		length = 0;
+		goto respond;
+		//goto stall;
 		break;
 
 	case ((USB_DIR_IN | USB_TYPE_STANDARD | USB_RECIP_INTERFACE) << 8
